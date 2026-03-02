8a693c9c3354c9187f293d86b0ac1338a70f65bf
tsdcoffice <tsdcoffice3@gmail.com>
2026-03-02 16:06:41 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tsdc_monitoring',
+  webDir: 'dist'
+};
+
+export default config;
